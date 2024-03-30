ce27b0824425fdf8d63b4d3f316cd21b12bbc1f1
Yuta Nishi <riyaamemiya@gmail.com>
2024-03-30 12:12:54 +0900

    refactor(DatabaseSeeder): 不要なOmikujiProbabilitySeeder呼び出しを削除

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,5 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        $this->call(OmikujiProbabilitySeeder::class);
     }
 }
